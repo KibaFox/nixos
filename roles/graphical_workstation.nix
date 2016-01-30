64d8983ecef43a052d7987b961edd762b306446e
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-01-30 14:54:07 -0500

    Enable Flash and WideVine on Chromium
    Some sites still require Flash and Netflix requires WideVine.
    
    As a note, I use the Flashcontrol plugin in Chromium to prevent Flash
    from starting automatically.

--- roles/graphical_workstation.nix
+++ roles/graphical_workstation.nix
@@ -62,4 +62,9 @@
     pkgs.xdotool          # Fake keyboard/mouse input, window management
     pkgs.xsel             # Clipboard manipulation
   ];
+
+  nixpkgs.config.chromium = {
+    enablePepperFlash = true;       # Some sites still need flash
+    enableWideVine = true;          # Allows Netflix to be played on Linux
+  };
 }
