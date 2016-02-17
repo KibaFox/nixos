47b2fd40317001deb6e6ece7ea20c4eb8afd73f4
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-02-17 14:43:28 -0500

    Update neopkgs.nix with the latest nixpkgs

--- mypkgs/neopkgs.nix
+++ mypkgs/neopkgs.nix
@@ -7,6 +7,6 @@ in
 import (pkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "8f793ce6bb050252e92a987cc3c5bd48d5423a42";
-    sha256 = "0fydxirp79s4svr2gadp3hsx2vrank904n8c5kprqh3jrzid90cg";
+    rev = "57b3ad896222abe307c5e20f4dfd7245db6bdb4d";
+    sha256 = "1n3nrfyjdlprimj1jdx8siml9rrjw0kzwv8r101syfg4dn41qvk1";
 }) ( args )
