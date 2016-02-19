f1b9575bf6df15320399f6a0cfc9c8751b69822e
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-02-18 20:17:18 -0500

    Add a little note about canto

--- roles/canto_rss_user.nix
+++ roles/canto_rss_user.nix
@@ -1,3 +1,8 @@
+# Canto is an Atom/RSS feed reader for the console that is meant to be quick,
+# concise, and colorful.
+#
+# Manual: http://codezen.org/canto-ng/manual/
+
 { config, pkgs, ... }:
 
 {
