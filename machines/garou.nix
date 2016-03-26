c901929315cae6bc808813a0d0f29923fe3844ba
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-03-26 13:09:39 -0400

    New machine: garou

---
+++ machines/garou.nix
@@ -0,0 +1,56 @@
+# garou - Thinkpad T530
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ../bundles/vdesktop.nix
+  ];
+
+  networking = {
+    hostName = "garou";
+    wireless.enable = true;
+  };
+
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ehci_pci"
+    "ata_piix"
+    "firewire_ohci"
+    "usb_storage"
+  ];
+
+  boot.kernelModules = [
+    "kvm-intel"
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_4_3;
+
+  boot.loader.grub.device = "/dev/sda";
+
+  boot.initrd.luks.devices = [{
+    name = "luksroot";
+    device = "/dev/sda2";
+    allowDiscards = true;
+  }];
+
+  fileSystems."/" = {
+    device = "/dev/mapper/garou1-root";
+    fsType = "btrfs";
+    options = "discard,compress=lzo";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "ext2";
+  };
+
+  swapDevices = [
+    { device = "/dev/mapper/garou1-swap"; }
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.maxJobs = 4;
+}
