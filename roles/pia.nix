675427bce99adc34f93a60fb8b7054ae9400aec7
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-08-16 13:03:30 -0400

    Update Private Internet Access (pia) role
    The OpenVPN configuration for PIA needed to be updated.  I downloaded
    the configuration from their site to update it.  Also, needed to specify
    the "up" and "down" parts to update the DNS from OpenVPN.

--- roles/pia.nix
+++ roles/pia.nix
@@ -13,22 +13,27 @@
           client
           dev tun
           proto udp
-          remote us-east.privateinternetaccess.com 1194
+          remote us-east.privateinternetaccess.com 1198
           resolv-retry infinite
           nobind
           persist-key
           persist-tun
-          ca /root/pia-ca.crt
+          cipher aes-128-cbc
+          auth sha1
           tls-client
           remote-cert-tls server
           auth-user-pass
           comp-lzo
           verb 1
           reneg-sec 0
-          crl-verify /root/pia-crl.pem
+          crl-verify /root/crl.rsa.2048.pem
+          ca /root/ca.rsa.2048.crt
+          disable-occ
           auth-user-pass /root/pia-login.conf
         '';
         autoStart = false;
+        up = "echo nameserver $nameserver | ${pkgs.openresolv}/sbin/resolvconf -m 0 -a $dev";
+        down = "${pkgs.openresolv}/sbin/resolvconf -d $dev";
       };
     };
   };
